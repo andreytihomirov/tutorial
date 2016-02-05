439e1e6cc8b6f84d87ba036b3333e0b70eb3e851
andrey.tihomirov <andrey.tihomirov@axamit.com>
2016-02-05 13:22:43 +0300

    Deleted unneeded logger

--- bundle/src/main/java/com/tutorial/components/searchPages/model/SearchPagesModel.java
+++ bundle/src/main/java/com/tutorial/components/searchPages/model/SearchPagesModel.java
@@ -4,8 +4,6 @@ import com.tutorial.components.searchPages.services.SearchPagesService;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
