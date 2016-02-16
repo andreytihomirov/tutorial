116e12dae16fe08eb10fc34d2e78b95a91e1d863
andrey.tihomirov <andrey.tihomirov@axamit.com>
2016-02-16 17:43:19 +0300

    Custom xtype element created.

---
+++ bundle/src/main/java/com/tutorial/components/customMultifield/model/Item.java
@@ -0,0 +1,26 @@
+package com.tutorial.components.customMultifield.model;
+
+/**
+ * Created by andrey.tihomirov on 16.02.2016.
+ */
+public class Item {
+
+    private String comboBox;
+    private String textField;
+
+    public String getComboBox() {
+        return comboBox;
+    }
+
+    public void setComboBox(String comboBox) {
+        this.comboBox = comboBox;
+    }
+
+    public String getTextField() {
+        return textField;
+    }
+
+    public void setTextField(String textField) {
+        this.textField = textField;
+    }
+}
